28ddabad9e4c2bc50bdc2fea93afd3f54cd5cfe1
TadeoOL <112605779+TadeoOL@users.noreply.github.com>
2022-09-13 21:37:14 -0700

    Add files via upload

--- CompiPrueba/Compi/Sintatico.cs
+++ CompiPrueba/Compi/Sintatico.cs
@@ -144,6 +144,150 @@ namespace Compi
             }
         }
 
+        private void statement()
+        {
+            switch (listaToken.ElementAt<Token>(p).ValorToken)
+            {
+                case 113: //:=
+                     simpleStatement();
+                    break;
+                case 208: //read
+                    simpleStatement();
+                    break;
+                case 209: //write
+                    simpleStatement();
+                    break;
+                case 210: // if
+                    structuredStatement();
+                    break;
+                case 213: // while
+                    structuredStatement();
+                    break;
+                default: 
+                    break;
+            }
+        }
+
+        private void simpleStatement()
+        {
+            switch (listaToken.ElementAt<Token>(p).ValorToken)
+            {
+                case 113: // :=
+                    assignmentStatement();
+                    break;
+                case 208: //read
+                    readStatement();
+                    break;
+                case 209: //write
+                    writeStatement();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void structuredStatement()
+        {
+            switch (listaToken.ElementAt<Token>(p).ValorToken)
+            {
+                case 210: //if
+                    conditionalStatement();
+                    break;
+                case 213: //while
+                    repetitiveStatement();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void assignmentStatement()
+        {
+            p++;
+            variable();
+            if (listaToken.ElementAt<Token>(p).ValorToken == 113) // :=
+            {
+                p++;
+                expression();
+            }
+            else
+            {
+                listaError.Add(ManejoErrores(500, listaToken.ElementAt<Token>(p).Linea)); // arreglar error al final
+            }
+        }
+
+        private void readStatement()
+        {
+            p++;
+            if (listaToken.ElementAt<Token>(p).ValorToken == 208) // read
+            {
+                p++;
+                variable();
+                while (listaToken.ElementAt<Token>(p).ValorToken == 117) // ,
+                {
+                    p++;
+                    variable();
+                }
+            }
+            else
+            {
+                listaError.Add(ManejoErrores(500, listaToken.ElementAt<Token>(p).Linea));
+            }
+        }
+
+        private void writeStatement()
+        {
+            p++;
+            if (listaToken.ElementAt<Token>(p).ValorToken == 209) // write
+            {
+                p++;
+                variable();
+                while (listaToken.ElementAt<Token>(p).ValorToken == 117) //,
+                {
+                    p++;
+                    variable();
+                }
+            }
+            else
+            {
+                listaError.Add(ManejoErrores(500, listaToken.ElementAt<Token>(p).Linea)); //arreglar errores al final
+            }
+        }
+
+        private void conditionalStatement()
+        {
+            ifStatement();
+        }
+
+        private void repetitiveStatement()
+        {
+            whileStatement();
+        }
+
+        private void variable()
+        {
+            entireVariable();
+        }
+
+        private void expression()
+        {
+
+        }
+
+        private void ifStatement()
+        {
+
+        }
+
+        private void whileStatement()
+        {
+
+        }
+
+        private void entireVariable()
+        {
+
+        }
         private Error ManejoErrores(int estado, int linea)
         {
             string mensajeError;
